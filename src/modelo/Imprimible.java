01cd234610b36bdc9dfd04fc46bfa391feec3fa9
rafalanda123 <rafalamda4@outlook.com>
2016-08-31 23:08:36 -0500

    Delete Imprimible.java

--- src/modelo/Imprimible.java
+++
@@ -1,7 +0,0 @@
-package modelo;
-
-public interface Imprimible {
-    void imprimirInformacion (String arg);
-    void showAdviceToServer (String advice);
-    void isConnectedToServer (boolean conn);
-}
